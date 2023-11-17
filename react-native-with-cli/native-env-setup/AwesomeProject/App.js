b83e2829c02c83965bb64afb46f8b016e9cac636
sumitMac <sumit.kumar@Technoidentity.com>
2023-11-17 16:19:44 +0530

    react-router-native added

--- react-native-with-cli/native-env-setup/AwesomeProject/App.js
+++ react-native-with-cli/native-env-setup/AwesomeProject/App.js
@@ -1,111 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  ScrollView,
+  TextInput,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-function Section({children, title}) {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import * as React from 'react';
+import {NativeRouter} from 'react-router-native';
 
 export const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <NativeRouter>
+      <ScrollView style={styles.container}>
+        <View style={styles.mainContainer}>
+          <View style={styles.header}>
+            <Text style={styles.hello}>Amazon MarketPlace</Text>
+            <TextInput placeholder="search here" style={styles.search_input} />
+          </View>
+          <Button
+            onPress={() => {
+              console.log('You tapped the button!');
+            }}
+            title="Click Me"
+          />
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </NativeRouter>
   );
 };
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: '#e3e6e6',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  mainContainer: {
+    flexDirection: 'column',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  header: {backgroundColor: '#131921'},
+  hello: {
+    fontSize: 20,
+    color: 'white',
+    margin: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  search_input: {
+    backgroundColor: '#ffffff',
   },
 });
